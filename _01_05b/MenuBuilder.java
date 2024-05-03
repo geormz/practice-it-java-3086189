602e5a45b8b9c34600b818e204f249f4154d6ae2
geormz <113058235+geormz@users.noreply.github.com>
2024-05-03 21:18:17 +0000

    complete course

--- _01_05b/MenuBuilder.java
+++ _01_05b/MenuBuilder.java
@@ -7,26 +7,29 @@ public class MenuBuilder {
 
     // Create a variable called menuTitle of type String and assign it the value "My
     // Dream Menu:".
+    String menuTitle = "My Dream menu";
 
     // Print the menuTitle variable to the console.
-
+    System.out.println(menuTitle);
     // Create a variable called menu of type ArrayList.
-
+    ArrayList<MenuItem> menu = new ArrayList<>();
     // Create a variable called starter of type MenuItem and pass in the name of
     // your favourite starter.
+    MenuItem starte = new MenuItem("Cocina");
 
     // Add the starter variable to the ArrayList called menu.
-
+    menu.add(starte);
     // Create a variable called mainCourse of type MenuItem and pass in the name of
     // your favourite main course.
-
+    MenuItem mianCourse = new MenuItem(" Soup");
     // Add the mainCourse variable to the ArrayList called menu.
-
+    menu.add(mianCourse);
     // Create a variable called dessert of type MenuItem and pass in the name of
     // your favourite dessert.
-
+    MenuItem dessert = new MenuItem("Cake");
     // Add the dessert variable to the ArrayList called menu.
-
+    menu.add(dessert);
     // Print the menu variable to the console.
+    System.out.println(menu);
   }
 }
--- _03_07b/EnhancedForLoops.java
+++ _03_07b/EnhancedForLoops.java
@@ -8,13 +8,27 @@ public class EnhancedForLoops {
   public static void main(String[] args) {
     int[] primeNumbers = { 2, 3, 5, 7, 11, 13, 17, 19 };
     // Write an enhanced for loop to print out each prime number in the array.
+    System.out.println("Prime number");
+    for (int p : primeNumbers) {
+      System.out.println(p);
+    }
 
+    System.out.println("Days of week");
     List<String> weekDays = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
     // Write an enhanced for loop to print out each week day in the list.
+    for (String weeks : weekDays) {
+      System.out.println(weeks);
 
+    }
+    System.out.println("Numbers greater than 50");
     int[] randomNumbers = { 23, 51, 72, 84, 1, 60, 34, 102 };
     // Write an enhanced for loop to print out the numbers in the array that are
     // greater than 50.
+    for (int n : randomNumbers) {
+      if (n > 50) {
+        System.out.println(n);
+      }
+    }
 
   }
 
